90561dfce91e077bd3539fdf7ea647278f621c48
Elizaveta Onatsko <elizavetaonatsko@Elizavetas-MacBook-Pro.local>
2018-06-15 10:51:30 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,51 @@
 
-var computerChoices = ["a", "b", "c","d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x", "z","y"];
-    var wins = 0;
-    var losses = 0;
-    var tries = 9;
-    var letterTried = [];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z", "y"];
+var wins = 0;
+var losses = 0;
+var tries = 9;
+var letterTried = [];
 
-    document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-        var userGuess = event.key;
-  
-        // Logic
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        if (userGuess === computerGuess || userGuess !== computerGuess ) {
-            tries--;
-            if(userGuess!==computerGuess && (tries!==9)){
-                letterTried+= userGuess + ", ";
-            }
-            if(tries===0 && (userGuess !== computerGuess)){
-                losses++;
-                tries=9;
-                letterTried=[];
-                }
-                if(tries===0 && (userGuess === computerGuess)){
-                    wins++;
-                    tries=9;
-                    letterTried=[];
-                    }
-            if (userGuess===computerGuess){
-                wins++;}
+    var userGuess = event.key;
 
-
-                
-            }
-            
-            
-
-            var html =
-            "<h1>The Psychic Game</h1>"+
-            "<p>Guess what letter I am thinking of</p>"+
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "</p>" +
-            "<p>Guesses Left: " + tries + "</p>"+
-            "<p>Your guesses so far: " + letterTried +" </p>";
-  
-          // Set the inner HTML contents of the #game div to our html string
-          document.querySelector("#game").innerHTML = html;
+    // Logic
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    if (userGuess === computerGuess || userGuess !== computerGuess) {
+        tries--; //always decrease number of tries
+        if (userGuess !== computerGuess && (tries !== 9)) {
+            letterTried += userGuess + ", "; //if the user guessed incorrectly and still had tries left, add letter to your guesses
         }
-    
-    
+        if (tries === 0 && (userGuess !== computerGuess)) { //if there are no more tries left and you guessed incorrectly, reset game
+            losses++;
+            tries = 9;
+            letterTried = [];
+        }
+        if (tries === 0 && (userGuess === computerGuess)) { //if there are no more tries left and u guessed correctly, add win and reset
+            wins++;
+            tries = 9;
+            letterTried = [];
+        }
+        if (userGuess === computerGuess) { //if you guessed right, add wins
+            wins++;
+        }
+
+
+
+    }
+
+
+
+    var html =
+        "<h1>The Psychic Game</h1>" +
+        "<p>Guess what letter I am thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + tries + "</p>" +
+        "<p>Your guesses so far: " + letterTried + " </p>";
+
+    //html code will access var html code
+    document.querySelector("#game").innerHTML = html;
+}
+
+
